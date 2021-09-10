0111f5d9e3b90df956e9b58f6e1d33ca35638ed7
Matías Gutiérrez <matyuh@hotmail.com>
2021-09-10 10:16:42 -0300

    added compile and deploy js files + basic test

---
+++ accounts_variables.js
@@ -0,0 +1,2 @@
+export const account = 'piano try obtain room wash bus ranch balcony project spy urge such';
+export const infuraUrl = 'https://rinkeby.infura.io/v3/3351e6d47432463dbe2c6e36e6f6cc85'
